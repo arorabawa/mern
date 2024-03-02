d1a211543fcd0d7a062611d2b14d6a77d0eb473f
arorabawa <arorabawa12345@gmail.com>
2024-03-02 16:13:08 +0530

    Chnage greetings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send({hi: 'there', });
+    res.send({Hello: 'buddy', });
 });
 
 const port  = process.env.PORT || 3000; //want known
