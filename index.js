87f3259ec765ad91b683450f464db6ea38ddd7ac
arorabawa <arorabawa12345@gmail.com>
2024-03-02 15:42:40 +0530

    new project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send({hi: 'there', });
+});
+
+const port  = process.env.PORT || 3000; //want known
+app.listen(port)
